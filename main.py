e089d7b8a6d3cb35ee3000bf273415c7c6531c8e
Zolty. <zoltypaypal@gmail.com>
2024-08-05 21:34:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# Filename: c:\Users\zolty\source\Github\Be-a-good-friend\main.py
+# Path: c:\Users\zolty\source\Github\Be-a-good-friend
+# Created Date: Monday, August 5th 2024, 9:32:12 pm
+# Author: Zolty
+# 
+# Copyright (c) 2024 SDA
+###
